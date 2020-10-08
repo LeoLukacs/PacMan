1cdc0259910c95af552d35784cdfabca56b9a4ea
Leo Lukacs <leolukacsls@gmail.com>
2020-10-08 19:54:25 +0100

    Added Teleport Pads
    The player and ghost can now teleport from right to left when they are trying to leave the map

---
+++ Assets/Scripts/TeleportPad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPad : MonoBehaviour
+{
+    public Vector2 sendToPos = Vector2.zero;
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        coll.gameObject.transform.position = sendToPos;
+    }
+
+}
