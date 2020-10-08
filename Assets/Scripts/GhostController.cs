7d6be116e81552ad5ec9d43fd8e1250603d1aa57
Leo Lukacs <leolukacsls@gmail.com>
2020-10-08 19:39:04 +0100

    Added Other Ghosts and Fixed Their Movement

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
-    public float speed = 1.0f; //The speed that the ghost can travel at.
-    public Vector2 direction = Vector2.up;// The direction the ghosts faces.
-    
-    //References to rigidbody2d and circle collider.
+
+    public float speed = 1.0f;
+
+    private Vector2 direction = Vector2.up;
+    private float directionChangeTime = 0.0f;//the next time the ghost may change direction
+
     private Rigidbody2D rb2d;
     private CircleCollider2D cc2d;
-    
-    
-    // Start is called before the first frame update
+
+    // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -22,13 +23,36 @@ public class GhostController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Eye Rotation sync with movement
-        foreach(Transform t in GetComponentInChildren<Transform>())
+        //Direction Change
+        if (!openDirection(direction))
         {
-            t.up = direction;
+            if (canChangeDirection())
+            {
+                changeDirection();
+            }
+            else if (rb2d.velocity.magnitude < speed)
+            {
+                changeDirectionAtRandom();
+            }
         }
-
-       //Movement
+        else if (canChangeDirection() && Time.time > directionChangeTime)
+        {
+            changeDirectionAtRandom();
+        }
+        //stuck on a non-wall
+        else if (rb2d.velocity.magnitude < speed)
+        {
+            changeDirectionAtRandom();
+        }
+        //Rotate Eyes
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t != transform)
+            {
+                t.up = direction;
+            }
+        }
+        //Move
         rb2d.velocity = direction * speed;
         if (rb2d.velocity.x == 0)
         {
@@ -37,6 +61,72 @@ public class GhostController : MonoBehaviour
         if (rb2d.velocity.y == 0)
         {
             transform.position = new Vector2(transform.position.x, Mathf.Round(transform.position.y));
+        }
+    }
+
+    private bool canChangeDirection()
+    {
+        Vector2 perpRight = Utility.PerpendicularRight(direction);
+        bool openRight = openDirection(perpRight);
+        Vector2 perpLeft = Utility.PerpendicularLeft(direction);
+        bool openLeft = openDirection(perpLeft);
+        return openRight || openLeft;
+    }
+
+    private void changeDirectionAtRandom()
+    {
+        directionChangeTime = Time.time + 1;//can't change directions for a second
+        if (Random.Range(0, 2) > 0)
+        {
+            changeDirection();
+        }
+    }
+
+    private void changeDirection()
+    {
+        directionChangeTime = Time.time + 1;//can't change directions for a second
+        Vector2 perpRight = Utility.PerpendicularRight(direction);
+        bool openRight = openDirection(perpRight);
+        Vector2 perpLeft = Utility.PerpendicularLeft(direction);
+        bool openLeft = openDirection(perpLeft);
+        if (openRight || openLeft)
+        {
+            int choice = Random.Range(0, 2);
+            if (!openLeft || (choice == 0 && openRight))
+            {
+                direction = perpRight;
+            }
+            else
+            {
+                direction = perpLeft;
+            }
+        }
+        else
+        {
+            direction = -direction;
+        }
+    }
+
+    private bool openDirection(Vector2 dir)
+    {
+        RaycastHit2D[] rch2ds = new RaycastHit2D[10];
+        float dist = 1;
+        cc2d.Cast(dir, rch2ds, dist, true);
+        foreach (RaycastHit2D rch2d in rch2ds)
+        {
+            if (rch2d && rch2d.collider.gameObject.tag == "Level")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.transform.position = new Vector2(0, 0);
         }
     }
 }
